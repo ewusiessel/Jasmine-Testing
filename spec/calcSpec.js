da3dd3a7e3edd20159de183fd54b3a6330b2d2fd
Ebenezer Ewusi-Essel <ewusiessel@gmail.com>
2020-02-15 11:20:39 +0000

    modified jasmine cdn in html file

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -2,6 +2,6 @@ describe ("Calculator", function(){
     describe ("Addition tests", function(){
         it ("should return 42", function(){
             expect (addition (20, 22)).toBe(42);
-        })
-    })
-})
+        });
+    });
+});
